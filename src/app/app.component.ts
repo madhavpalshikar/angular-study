45431f074fff358ba295a94826c8a86e99223ce8
Madhav <mpdebug200@gmail.com>
2021-03-18 20:00:30 +0530

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+//import area
 import { Component } from '@angular/core';
 
+//component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
+//component logic
 export class AppComponent {
   title = 'my-app';
 }
